9f3e84aa5ad58bed6490ec0f32f96118416d077f
Budi Erwanto <budi.erwanto@campaign.com>
2020-12-09 21:56:08 +0700

    Fix Encryption

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ docker push sariroti/docker-react-multicontainer-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=sariroti/docker-react-multicontainer-server:$SHA
-kubectl set image deployments/client-deployment server=sariroti/docker-react-multicontainer-client:$SHA
-kubectl set image deployments/worker-deployment server=sariroti/docker-react-multicontainer-worker:$SHA
+kubectl set image deployments/client-deployment client=sariroti/docker-react-multicontainer-client:$SHA
+kubectl set image deployments/worker-deployment worker=sariroti/docker-react-multicontainer-worker:$SHA
