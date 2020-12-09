f3aa9c37e930fcf6ae574a2f7c560781f13a69de
Budi Erwanto <budi.erwanto@campaign.com>
2020-12-09 22:31:08 +0700

    Fix Encryption

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
-docker build -t sariroti/docker-react-multicontainer-client:latest -t sariroti/docker-react-multicontainer-client:$SHA ./client/Dockerfile ./client
-docker build -t sariroti/docker-react-multicontainer-server:latest -t sariroti/docker-react-multicontainer-server:$SHA  ./server/Dockerfile ./server
-docker build -t sariroti/docker-react-multicontainer-worker:latest -t sariroti/docker-react-multicontainer-worker:$SHA ./worker/Dockerfile ./worker
+docker build -t sariroti/docker-react-multicontainer-client:latest ./client/Dockerfile ./client
+docker build -t sariroti/docker-react-multicontainer-server:latest ./server/Dockerfile ./server
+docker build -t sariroti/docker-react-multicontainer-worker:latest ./worker/Dockerfile ./worker
 docker push sariroti/docker-react-multicontainer-client:latest
 docker push sariroti/docker-react-multicontainer-server:latest
 docker push sariroti/docker-react-multicontainer-worker:latest
-docker push sariroti/docker-react-multicontainer-client:$SHA
-docker push sariroti/docker-react-multicontainer-server:$SHA
-docker push sariroti/docker-react-multicontainer-worker:$SHA
+
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=sariroti/docker-react-multicontainer-server:$SHA
-kubectl set image deployments/client-deployment client=sariroti/docker-react-multicontainer-client:$SHA
-kubectl set image deployments/worker-deployment worker=sariroti/docker-react-multicontainer-worker:$SHA
+kubectl set image deployments/server-deployment server=sariroti/docker-react-multicontainer-server:latest
+kubectl set image deployments/client-deployment client=sariroti/docker-react-multicontainer-client:latest
+kubectl set image deployments/worker-deployment worker=sariroti/docker-react-multicontainer-worker:latest
